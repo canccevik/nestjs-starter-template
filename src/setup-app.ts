3b68dfb576aeabf8ce6d3fc09c738be47fe761e3
Can Çevik <mazlumcancevik@gmail.com>
2023-07-08 09:15:13 +0300

    feat: add logging interceptor

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -4,8 +4,9 @@ import { AppModule } from '@modules/app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { useContainer } from 'class-validator'
 import { Config, ENV } from '@config/index'
-import { TransformInterceptor } from '@core/interceptors'
+import { LoggingInterceptor, TransformInterceptor } from '@core/interceptors'
 import { Reflector } from '@nestjs/core'
+import { Logger } from 'nestjs-pino'
 import mongoSanitize from 'express-mongo-sanitize'
 import compression from 'compression'
 import helmet from 'helmet'
@@ -24,4 +25,9 @@ export function setupApp(app: NestExpressApplication): void {
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new TransformInterceptor(new Reflector()))
+
+  if (!config.isTest) {
+    const logger = app.get<Logger>(Logger)
+    app.useGlobalInterceptors(new LoggingInterceptor(logger))
+  }
 }
