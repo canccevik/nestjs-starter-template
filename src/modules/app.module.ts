cd5709153f7181ce78751531049868851fc90b82
Can Çevik <mazlumcancevik@gmail.com>
2023-07-09 13:28:14 +0300

    feat: add features module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,9 +3,11 @@ import { EnvalidModule } from 'nestjs-envalid'
 import { validators } from '@config/index'
 import { DatabaseModule } from './database/database.module'
 import { LoggerModule } from './logger/logger.module'
+import { FeaturesModule } from '@features/features.module'
 
 @Module({
   imports: [
+    FeaturesModule,
     EnvalidModule.forRoot({
       validators,
       isGlobal: true,
